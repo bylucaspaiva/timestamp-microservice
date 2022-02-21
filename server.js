7de740f60e3cb6e50e92d5bf1c21e0676965f81d
lspaiva <lucasp4iva@gmail.com>
2022-02-21 01:38:01 -0300

    tidying up heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+//app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -31,10 +31,30 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+/* ############################# below is my implementation */
+
+
 let resObject = {};
 
 app.get('/api/:date', (req, res) => {
     let date = req.params.date;
+    if(date.includes(' ') || date.includes('-')) {
+      resObject['unix'] = new Date(date).getTime();
+      resObject['utc'] = new Date(date).toUTCString();
+    }else{
+        date = parseInt(date);
+        resObject['unix'] = new Date(date).getTime();
+        resObject['utc'] = new Date(date).toUTCString();
+    }
+    if(!resObject['unix'] || !resObject['utc']) {
+         res.json({error: "Invalid Date"});
+    }
+    res.json(resObject);
+});
 
-    res.json(date);
+app.get('/api', (req,res) => {
+    resObject['unix'] = new Date().getTime();
+    resObject.utc = new Date().toUTCString();
+
+    res.json(resObject);
 })
